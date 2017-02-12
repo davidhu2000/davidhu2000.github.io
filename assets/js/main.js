ec2ca017297898b60e8ccb0a195fc8a8940bcf7e
David Hu <davidhu314@gmail.com>
2017-02-12 11:51:22 -0800

    minor fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -72,7 +72,7 @@
 
 					$body._reposition = function() {
 						if (skel.vars.touch && (window.orientation == 0 || window.orientation == 180))
-							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
+							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height() - 100, 30) + 'px');
 						else
 							$wrapper.css('padding-top', ((($window.height() - panels[firstPanelId].height()) / 2) - $nav.height()  - 100) + 'px');
 					};
