fd8d6277935919820a4a8ef6f1f3406aafb94d9e
David Hu <davidhu314@gmail.com>
2022-08-23 20:55:50 -0700

    fix

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE = {
-  name: "My personal website.",
+  name: "David Hu",
   domain: "https://davidhu.io",
   github: "https://github.com/davidhu2000",
   description: "Welcome to my website!",
