b4893bb7a0551967a6bbf6f7c72541298fd68683
David Hu <davidhu314@gmail.com>
2022-08-27 21:30:52 -0700

    create landing and about pages (#11)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,11 @@
 import rss from "@astrojs/rss";
 import { SITE } from "../config";
 
-export const get = () =>
-  rss({
+export const get = () => {
+  return rss({
     title: SITE.name,
     description: SITE.description,
     site: import.meta.env.SITE,
     items: import.meta.glob("./blog/**/*.md"),
   });
+};
