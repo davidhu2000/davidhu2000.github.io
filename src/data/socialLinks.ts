e68e4743a9cc3c9bb2c8440e99cd337b695bd46c
David Hu <davidhu314@gmail.com>
2022-08-30 20:39:54 -0700

    create contact page (#20)

---
+++ src/data/socialLinks.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/in/davidhu2000/",
+    icon: "mdi:linkedin",
+  },
+  {
+    label: "Github",
+    url: "https://github.com/davidhu2000/davidhu2000.github.io",
+    icon: "mdi:github",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/notdavidhu",
+    icon: "mdi:twitter",
+  },
+  {
+    label: "AngelList",
+    url: "https://angel.co/davidhu2000",
+    icon: "fa-brands:angellist",
+  },
+];
