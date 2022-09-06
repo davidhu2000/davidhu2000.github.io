f82cb2fbb354c7b17bd9e3c113c63881aca18e9e
David Hu <davidhu314@gmail.com>
2022-09-05 18:10:37 -0700

    integrate with astro image to optimize images (#39)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
+import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://davidhu.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), image()],
 });
