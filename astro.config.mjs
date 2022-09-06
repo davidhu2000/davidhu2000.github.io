b55fafd02db7bc6eeedc0d32ad9e5660efcf1ad4
David Hu <davidhu314@gmail.com>
2022-09-06 08:14:53 -0700

    switch formatter to prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import image from "@astrojs/image";
-import tailwind from "@astrojs/tailwind";
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://davidhu.io",
+  site: 'https://davidhu.io',
   integrations: [mdx(), sitemap(), tailwind(), image()],
 });
