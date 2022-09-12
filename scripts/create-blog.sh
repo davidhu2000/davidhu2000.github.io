2ab34be1d377e73921f072f8deae3b80a5479639
David Hu <davidhu314@gmail.com>
2022-09-11 20:52:16 -0700

    improve creat blog script

--- scripts/create-blog.sh
+++ scripts/create-blog.sh
@@ -6,16 +6,20 @@ blog=$(echo $blog | tr ' ' -)
 
 echo "setting blog with name $blog"
 
+git checkout main
+git pull 
+git checkout -b blog-$blog
+
 mkdir public/blog/$blog
 touch src/pages/blog/$blog.mdx
 
-
 cat > src/pages/blog/$blog.mdx << EOF
+---
 layout: "../../layouts/BlogPost.astro"
-title: "<TODO>"
-description: "<TODO>"
-pubDate: "<TODO>"
-heroImage: "/blog/$blog/hero-image.webp"
+title: ""
+description: ""
+pubDate: ""
+heroImage: "./src/assets/blog/$blog/hero-image.webp"
 tags: []
 ---
 
