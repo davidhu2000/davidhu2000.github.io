b6e4c222089972d06d318d71c5046c0973ced166
David Hu <davidhu314@gmail.com>
2022-09-11 21:45:22 -0700

    Revert "show reading time on blog posts" (#46)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://davidhu.io",
   integrations: [mdx(), sitemap(), tailwind(), image()],
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-  },
 });
