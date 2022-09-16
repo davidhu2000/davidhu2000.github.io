2d46f2c48324b55311db369d10fd7f191ec6f72c
David Hu <davidhu314@gmail.com>
2022-09-15 22:05:35 -0700

    add prefetch to speed up site

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://davidhu.io",
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkReadingTime],
-    }),
-    sitemap(),
-    tailwind(),
-    image(),
-  ],
+  integrations: [mdx({
+    remarkPlugins: [remarkReadingTime]
+  }), sitemap(), tailwind(), image(), prefetch()]
 });
