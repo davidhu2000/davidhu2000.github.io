f043fa3a0ceb5731045d7e74666eb8867bcc3975
David Hu <davidhu314@gmail.com>
2022-09-26 21:50:52 -0700

    add line highlighting to code blocks (#52)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,44 @@ import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import rehypePrettyCode from "rehype-pretty-code";
 
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
+
+const options = {
+  theme: "github-dark",
+  // TODO: figure out how to toggle themes properly
+  // theme: {
+  //   dark: "github-dark",
+  //   light: "github-light",
+  // },
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{ type: "text", value: " " }];
+    }
+  },
+  // Feel free to add classNames that suit your docs
+  onVisitHighlightedLine(node) {
+    console.log("here");
+    console.log(node);
+    node.properties.className.push("highlighted");
+  },
+  onVisitHighlightedWord(node) {
+    node.properties.className = ["word"];
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.davidhu.io",
-  integrations: [
-    mdx({
-      remarkPlugins: [remarkReadingTime],
-    }),
-    sitemap(),
-    tailwind(),
-    image(),
-    prefetch(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind(), image(), prefetch()],
+  markdown: {
+    syntaxHighlight: false,
+    extendDefaultPlugins: true,
+    // TODO: this plugin does not work for mdx files, need to figure out why
+    rehypePlugins: [[rehypePrettyCode, options]],
+    remarkPlugins: [remarkReadingTime],
+  },
 });
